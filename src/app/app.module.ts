3ba3a38eace438a25aba08e199c8c8d2f86f540a
Enrique Comesaña <99300789+k1k3cb@users.noreply.github.com>
2023-04-26 12:07:46 +0200

    routing added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,41 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RopaComponent } from './components/ropa/ropa.component';
+import { CalzadoComponent } from './components/calzado/calzado.component';
+import { AccesoriosComponent } from './components/accesorios/accesorios.component';
+import {  RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+
+const APP_ROUTES: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'ropa', component: RopaComponent },
+  { path: 'calzado', component: CalzadoComponent },
+  { path: 'accesorios', component: AccesoriosComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavMenuComponent,
+    RopaComponent,
+    CalzadoComponent,
+    AccesoriosComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatToolbarModule,
+    RouterModule.forRoot(APP_ROUTES)
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
